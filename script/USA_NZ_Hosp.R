10d22a8a2dfe4b80a96be875d3ab01cbd9285e1a
Joaomuianga <joao.muianga1@gmail.com>
2023-11-07 23:37:51 -0300

    week44

--- script/USA_NZ_Hosp.R
+++ script/USA_NZ_Hosp.R
@@ -8,52 +8,31 @@
 #####################################################################
 pacman::p_load(
 
-  learnr,   # interactive tutorials
-  here,     # file paths relative to R project root folder
-  rio,      # import/export of many types of data
-  openxlsx, # special functions for handling Excel workbooks
-  remotes,  # install from github
+  here,         # file paths relative to R project root folder
+  rio,          # import/export of many types of data
+  openxlsx,     # special functions for handling Excel workbooks
+  remotes,      # install from github
   tidyverse,    # includes many packages for tidy data wrangling and presentation
   skimr,        # data exploration
   epikit,       # useful epi functions
   lubridate,    # dates and times
   stringr,      # handling characters
   base,
-  janitor,      # tables and data cleaning
-  gtsummary,    # making descriptive and statistical tables
+  janitor,     # tables and data cleaning
   corrr,
   caret,
   broom,
   httr,
-  jsonlite,
-  cowplot,          # combining plots  
-  RColorBrewer,     # color scales
-  gghighlight,      # highlight a subset
-  ggrepel,          # smart labels
-  ggExtra,          # fancy plots  
-  tsibble,          # epiweeks
-  viridis,          # colorblind-friendly scales
-  scales,           # helper functions
-  apyramid,         # age and sex pyramids
-  rmarkdown,        # produce PDFs, Word Documents, Powerpoints, and HTML files
-  knitr,            # R Markdown report generation and html tables
-  flextable)        # HTML tables
+  curl,
+  jsonlite)        
 
 
 # Install phi function to get all XMART data 
-devtools::install_github("whocov/phifunc", auth_token = "d9b68eb06316f7e322c660103b187cbbcf024f72", subdir = "phifunc", dep = TRUE, force = TRUE)
+devtools::install_github("whocov/phifunc", auth_token = "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", subdir = "phifunc", dep = TRUE, force = TRUE)
 # pull all data
-
 require(phifunc)
-require(tidyverse)
 
-# get daily cases/deaths
-
-phi_data <- pull_phi_data()
-
-# get all datasets
-# imports cases/death data and testing data by default - this includes phi data 
-
+# reference table with population data
 all_data <- read_all_data(
   use_ext_phi_data = FALSE, # using cases and deaths data from external dashboard
   stringency = TRUE, # import stringency data
@@ -63,9 +42,6 @@ all_data <- read_all_data(
   phsm = TRUE, # import public health and social measures index
   epinow = TRUE  # import epinow data (effective R number)
 )
-
-# reference table with population data
-pop_data <- pull_pop_data()
 
 #############################################################################################
 ### Import file of countries who have API GET json or Github
@@ -88,8 +64,8 @@ request_1$status_code  # check for any server error ("200" is good!)
 USA_api_1 <- base::rawToChar(request_1$content)
 USA_file_1 <- jsonlite::fromJSON(USA_api_1, flatten = TRUE)
 
-
 ##### Import NZ data
+library(readr)
 new_zealand_file_1<-read.csv("https://raw.githubusercontent.com/minhealthnz/nz-covid-data/main/cases/weekly-hospitalisations-for-covid.csv")
 
 ########## USA have 2 API with max of 50000000 row per API, to complete join both for one file
@@ -142,13 +118,12 @@ nz_data <- new_zealand_file_1%>%
 
 ###################################################################
 ## Join USA and NZ dataset in one file
-historical_data <- full_join(usa_data_full, nz_data, by=c("report_date", "country_name", "new_hospitalization"))
+#historical_data <- full_join(usa_data_full, nz_data, by=c("report_date", "country_name", "new_hospitalization"))
 
-#######################################
 ## clean historical data
-################################################################
+#########################
 ## remove  duplication
-historical_data <- historical_data%>%
+historical_data <- usa_data_full%>%
   group_by(report_date, country_name) %>%       # group the rows by 'name'
   slice_max(new_hospitalization,        # keep row per group with maximum date value 
             n = 1,         # keep only the single highest row 
@@ -165,8 +140,7 @@ historical_data <- historical_data%>%
     iso_year = year(report_date))%>%
   # sum column base on value number
   group_by(country_name, epiweek)%>%
-  summarise(new_hospitalization = if(all(is.na(new_hospitalization))) NA else sum(new_hospitalization, na.rm = T),
-            new_icu = if(all(is.na(new_icu))) NA else sum(new_icu, na.rm = T))
+  summarise(new_hospitalization = if(all(is.na(new_hospitalization))) NA else sum(new_hospitalization, na.rm = T))
 
 ########################################################
 historical_data<- historical_data%>%
@@ -175,25 +149,25 @@ historical_data<- historical_data%>%
     iso_week_number = week(epiweek))     #create week number columns
 
 historical_data <- historical_data%>%
-  select(country_name, iso_year, iso_week_number, epiweek, new_hospitalization, new_icu)%>%
+  select(country_name, iso_year, iso_week_number, epiweek, new_hospitalization)%>%
   rename(country = country_name)%>%
   mutate(country = recode( country,
-    "United States of America" = "UNITED STATES OF AMERICA",
-    "New Zealand"              = "NEW ZEALAND"))
+    "United States of America" = "UNITED STATES OF AMERICA"))
 
 
 ###########################################
 ### Join ref country and historical data
-historical_data <- left_join(historical_data, ref_places, by= ("country"))
+historical_data <- left_join(historical_data, ref_places, by= ( "country"))
 
 ##### calculate last week and incompleted 
-last_week = max(historical_data$epiweek)
+#last_week = max(historical_data$epiweek)
+
 
 # remove last week
-historical_data<- historical_data%>%filter(epiweek < last_week)
+#historical_data<- historical_data%>%filter(epiweek < last_week)
 #### Export data to Excel
 
-export(historical_data, here("USA_NZ_Hosp.csv"))
+export(historical_data, here("USA_Hosp.csv"))
 
 
 
